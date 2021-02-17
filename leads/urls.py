0529f3ba14f90d56aedf0ae173d5b1926977b32a
JP Crain <jp@bitcube.tech>
2021-02-17 15:29:29 +0200

    Basic set-up of leads CRM

---
+++ leads/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from leads.views import lead_list, lead_details, lead_create, lead_update, lead_delete
+
+app_name = "leads"
+
+urlpatterns = [
+    path('', lead_list, name='lead-list'),
+    path('create/', lead_create, name='lead-create'),
+    path('<int:pk>/', lead_details, name='lead-details'),
+    path('<int:pk>/update/', lead_update, name='lead-update'),
+    path('<int:pk>/delete/', lead_delete, name='lead-delete'),
+]
---
+++ leads/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Lead, Agent
+from .forms import LeadModelForm, LeadForm
+
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {"leads": leads}
+    # return HttpResponse('Hello World')
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_details(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {"lead": lead}
+    return render(request, "leads/lead_details.html", context)
+
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {"form": form}
+    return render(request, "leads/lead_create.html", context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "lead": lead,
+        "form": form
+    }
+    return render(request, "leads/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
