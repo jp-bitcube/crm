dad3fad72b55eece8387555686f5a25d42317697
JP Crain <jp@bitcube.tech>
2021-02-19 18:52:58 +0200

    change static files root

--- crm/settings.py
+++ crm/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
-STATIC_ROOT = "static_root"
+STATIC_ROOT = "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'leads.User'
