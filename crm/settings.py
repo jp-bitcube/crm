85b0fd3b13716900056ee973f3347a083c5fc83b
JP Crain <jp@bitcube.tech>
2021-02-19 16:53:24 +0200

    change settings

--- crm/settings.py
+++ crm/settings.py
@@ -30,13 +30,6 @@ SECRET_KEY = env('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
